99b2e069fa79035105a54cd22094ea13270b7469
Ramon Nunes Teixeira <ramon.teixeira@db1.com.br>
2019-05-18 11:42:45 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import './App.css';
+import PouchDB from 'pouchdb-browser';
+
+export default class App extends Component {
+  db = new PouchDB('todo');
+
+  constructor() {
+    super();
+
+    this.state = {
+      todos: []
+    }
+
+    this.list()
+
+    this.db.changes({live: true})
+          .on('change', () => this.list());
+
+    if (localStorage.remoteDB) {
+        this.db.replicate.to(localStorage.remoteDB, { live: true });
+    }
+  }
+
+  async list() {
+    const find = await this.db.allDocs({include_docs: true});
+    this.setState({ todos: find.rows.map(e => e.doc) });
+  }
+
+  async insert() {
+    const rand = Math.floor(Math.random() * (999 - 100) + 100);
+    await this.db.post({
+      name: `teste ${rand}`
+    });
+  }
+
+  async remove(item) {
+    await this.db.remove(item);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.insert.bind(this)}>
+              Incluir
+        </button>
+
+        <ul class="board">
+          {this.state.todos.map(
+              (item) => { 
+                  return (
+                    <li class="item" key={item._id}>{item.name}
+                      <button onClick={() => this.remove(item)}>x</button>
+                    </li>
+                  );
+              }
+          )}
+
+        </ul>
+      </div>
+    );
+  }
+}
